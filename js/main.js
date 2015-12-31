f325cea44cbe70c8d58949ba14c7617ef6694ac6
Benjamin Oakes <boakes@liaison-intl.com>
2015-12-31 10:01:39 -0600

    Wait for document to be ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// We're generating some of the Markdown we use, so apply styles dynamically.
-$('h2:first').addClass('page-header');
-$('p:first').addClass('lead');
-$('table').addClass('table table-striped');
+$(function () {
+  // We're generating some of the Markdown we use, so apply styles dynamically.
+  $('h2:first').addClass('page-header');
+  $('p:first').addClass('lead');
+  $('table').addClass('table table-striped');
+});
